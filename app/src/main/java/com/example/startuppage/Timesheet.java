22f2778dfffdb31a023284deeb6931e99c3550d3
JakeHessian <jake.124124@gmail.com>
2020-11-22 15:55:12 -0500

    going to use fragments to edit week1 and week2

--- app/src/main/java/com/example/startuppage/Timesheet.java
+++ app/src/main/java/com/example/startuppage/Timesheet.java
@@ -15,7 +15,7 @@ public class Timesheet extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timesheet);
+        setContentView(R.layout.timesheet_fragment_2);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
