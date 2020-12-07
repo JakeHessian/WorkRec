157b26a6555e21d277f5000f78183a165b26a843
JakeHessian <jake.124124@gmail.com>
2020-12-07 14:04:21 -0500

    now compiles

--- app/src/main/java/com/example/startuppage/MainActivity.java
+++ app/src/main/java/com/example/startuppage/MainActivity.java
@@ -8,23 +8,10 @@ import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private DatabaseReference mDatabase;
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-
 
     public Button btn;
     protected static final String ACTIVITY_NAME = "MainActivity";
@@ -33,20 +20,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.discussion_content);
-        //initialize recyclerview and FIrebase objects
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("DiscussionBoard");
-        mAuth = FirebaseAuth.getInstance();
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btnStart);
-
         btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.i(ACTIVITY_NAME, "User clicked Get Started");
@@ -58,57 +34,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-        FirebaseRecyclerAdapter<discussion_dblink, discussionViewHolder> FBRA = new FirebaseRecyclerAdapter<discussion_dblink, discussionViewHolder>(
-                discussion_dblink.class,
-                R.layout.discussion_home,
-                discussionViewHolder.class,
-                mDatabase
-        ) {
-            @Override
-            protected void populateViewHolder(discussionViewHolder viewHolder, discussion_dblink model, int position) {
-                final String post_key = getRef(position).getKey();
-                viewHolder.setTitle(model.getTitle());
-                viewHolder.setDesc(model.getDesc());
-                viewHolder.setUserName(model.getUsername());
-                viewHolder.mView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent singleActivity = new Intent(MainActivity.this, Login.class);
-                        singleActivity.putExtra("PostID", post_key);
-                        startActivity(singleActivity);
-                    }
-                });
-            }
-        };
-        recyclerView.setAdapter(FBRA);
-    }
-
-    public static class discussionViewHolder extends RecyclerView.ViewHolder {
-        View mView;
-
-        public discussionViewHolder(View itemView) {
-            super(itemView);
-            mView = itemView;
-        }
-
-        public void setTitle(String title) {
-            TextView post_title = mView.findViewById(R.id.post_title_txtview);
-            post_title.setText(title);
-        }
-
-        public void setDesc(String desc) {
-            TextView post_desc = mView.findViewById(R.id.post_desc_txtview);
-            post_desc.setText(desc);
-        }
-
-        public void setUserName(String userName) {
-            TextView postUserName = mView.findViewById(R.id.post_user);
-            postUserName.setText(userName);
-        }
-
-    }
 }
